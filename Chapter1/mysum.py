5325f3e448a2d7c8784530e3fd5a4e374b560ea9
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2020-03-01 22:05:06 -0800

    Updates to mysum, trying lists

--- Chapter1/mysum.py
+++ Chapter1/mysum.py
@@ -1,10 +1,12 @@
-def mysum(*args):
+def mysum(*numbers):
 
     result = 0
-    for _, val in enumerate(args):
+    for val in numbers:
         result += val
         
     return result
     
 print(f"1+2+3 = {mysum(1,2,3)}")
 print(f"4+5+6+3+2+1 = {mysum(4,5,6,3,2,1)}")
+print(f"9+99+999+9999 = {mysum(9,99,999,9999)}")
+print(f"mysum(*[5,25,55] = {mysum(*[5, 25, 55])}")
